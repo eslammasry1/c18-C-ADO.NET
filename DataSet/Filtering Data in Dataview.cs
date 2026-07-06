2e3ed091403012dd96e7516907fdb13d74a75915
eslammasry1 <eslam.mostafa.omer@gmail.com>
2026-07-06 09:51:16 +0300

    Implement employee data filtering in DataView
    This code creates a DataTable for employees, adds sample data, and filters the data to display only employees from Jordan and Egypt.

---
+++ DataSet/Filtering Data in Dataview.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Data;
+using System.Linq;
+namespace DataTableExample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable EmployeesDataTable = new DataTable();
+            EmployeesDataTable.Columns.Add("ID", typeof(int));
+            EmployeesDataTable.Columns.Add("Name", typeof(string));
+            EmployeesDataTable.Columns.Add("Country", typeof(string));
+            EmployeesDataTable.Columns.Add("Salary", typeof(Double));
+            EmployeesDataTable.Columns.Add("Date", typeof(DateTime));
+            EmployeesDataTable.Rows.Add(1, "Mohammed Abu-Hadhoud", "Jordan", 5000, DateTime.Now);
+            EmployeesDataTable.Rows.Add(2, "Ali Maher", "KSA", 525.5, DateTime.Now);
+            EmployeesDataTable.Rows.Add(3, "Lina Kamal", "Jordan", 730.5, DateTime.Now);
+            EmployeesDataTable.Rows.Add(4, "Fadi Jameel", "Egypt", 800, DateTime.Now);
+            EmployeesDataTable.Rows.Add(5, "Omar Mahmoud", "Lebanon", 7000, DateTime.Now);
+
+            DataView EmployeesDataView1 = EmployeesDataTable.DefaultView;
+
+            EmployeesDataView1.RowFilter = "Country='Jordan' or Country ='Egypt' ";
+            for (int i = 0; i < EmployeesDataView1.Count; i++)
+            {
+                Console.WriteLine("{0},{1},{2},{3},{4}",
+                EmployeesDataView1[i][0], EmployeesDataView1[i][1],
+                EmployeesDataView1[i][2], EmployeesDataView1[i][3], EmployeesDataView1[i][4]);
+            }
+        }
+    }
+}
